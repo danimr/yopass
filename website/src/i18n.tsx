f294b611bb70161887a4177f146da35d97f5c9fc
Dani Mora <dani@turingprojects.com>
2024-11-06 21:04:38 +0100

    branding 2

--- website/src/i18n.tsx
+++ website/src/i18n.tsx
@@ -12,7 +12,7 @@ i18n
       loadPath: process.env.PUBLIC_URL + '/locales/{{lng}}.json',
     },
 
-    fallbackLng: process.env.REACT_APP_FALLBACK_LANGUAGE || 'en',
+    fallbackLng: process.env.REACT_APP_FALLBACK_LANGUAGE || 'es',
     debug: false,
 
     // have a common namespace used around the full app
